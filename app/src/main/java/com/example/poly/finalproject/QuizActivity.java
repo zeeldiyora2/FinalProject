40027b05ab0ae93883c8b25018fa3624680b0d7d
Vijay <abhi0001@algonquinlive.com>
2018-04-19 00:36:26 -0400

    Quit button fixed

--- app/src/main/java/com/example/poly/finalproject/QuizActivity.java
+++ app/src/main/java/com/example/poly/finalproject/QuizActivity.java
@@ -1,13 +1,28 @@
 package com.example.poly.finalproject;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class QuizActivity extends Activity {
-
+public class QuizActivity extends Activity
+{
+    Button quitButton;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        quitButton = findViewById(R.id.btnquit);
+        quitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent quizIntend = new Intent(QuizActivity.this,MainActivity.class);
+                startActivityForResult(quizIntend,50);
+            }
+        });
     }
 }
